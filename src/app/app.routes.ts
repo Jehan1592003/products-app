3f3b2c1a3abaad8b7fe770d14f5f902604136e6e
Jehan1592003 <Jehan1592003@gmail.com>
2026-05-21 12:25:00 +0300

    add CSS styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,18 @@
+
 import { Routes } from '@angular/router';
 import { Product } from './features/products/component/product/product';
 import { About } from './static/component/about/about';
-import { ActiveForms } from './features/forms/forms/active-forms/active-forms';
-import { TemplateForm } from './features/forms/forms/active-forms/template-form/template-form';
-import { ProductList } from './features/product-list/component/component/product-list';
+import { ActiveForms } from './features/forms/component/active-form/active-form';
+import { TemplateForm } from './features/forms/component/template-form/template-form';
 import { Home } from './shared/component/home/home';
+import { ProductList } from './features/products/component/product-list/product-list/product-list';
 
 export const routes: Routes = [
-       { path: '', component: Home, },
-      { path: 'product-list', component: ProductList},
-       { path: 'active-forms', component: ActiveForms},
-       { path: 'template-form', component: TemplateForm},
-      { path: 'product/:id', component: Product },
-      { path: 'about', loadComponent: () => import('./static/component/about/about').then(m => m.About)},
-      { path: '**', redirectTo: '' }
-
-      
+  { path: '', component: Home },
+  { path: 'product-list', component: ProductList },
+  { path: 'active-forms', component: ActiveForms },
+  { path: 'template-form', component: TemplateForm },
+  { path: 'product/:id', component: Product },
+  { path: 'about', loadComponent: () => import('./static/component/about/about').then(m => m.About) },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { Footer } from './components/footer/footer';
+import { Navbar } from './shared/component/home/navbar/navbar';
+import { Footer } from './shared/component/home/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar, Footer],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('products-app');
-}
+export class App {}
